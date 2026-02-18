40c8ecb19f7caa1b475d782238eb4710cd0909ea
Muhammet Emre Numanoğlu <muhammedemrenumanoglu155@gmail.com>
2026-02-18 13:16:16 +0300

    Updated Numanoglu Insaat

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// const projelerimiz = document.querySelector("#projelerimiz");
+
+// runEventListeners();
+
+// function runEventListeners()
+// {
+//     projelerimiz.addEventListener("click",open);
+// }
+
+// function open()
+// {
+
+// }
+window.history.replaceState({}, document.title, window.location.pathname.replace('.html', ''));
